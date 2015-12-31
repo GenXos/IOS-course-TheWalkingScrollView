b8340493a27d8b0ade48520fd3e380a779447f81
Todd Fields <toddwfields@gmail.com>
2015-12-31 13:32:09 -0600

    completed scrollview project

--- TheWalkingScrollView/ViewController.swift
+++ TheWalkingScrollView/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var scrollView: UIScrollView!
+
+  let WIDTH: CGFloat = 223
+  let HEIGHT: CGFloat = 294
+  
   override func viewDidLoad() {
     super.viewDidLoad()
    
+    for var x = 1; x < 5; x++ {
+      let img = UIImage(named: "\(x)")
+      let imgView = UIImageView(image: img)
+      
+      scrollView.addSubview(imgView)
+      
+      imgView.frame = CGRectMake(-WIDTH + (WIDTH * CGFloat(x)), 320, WIDTH, HEIGHT)
+    }
+    
+    scrollView.contentSize = CGSizeMake(WIDTH * 5, scrollView.frame.size.height)
     
   }
 
